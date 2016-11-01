1a19b274931728d011ccdb536f828f68e818495c
Josha von Gizycki <mail@joshavg.de>
2016-11-01 23:12:44 +0100

    added some prime factor stuff

--- 3.clj
+++ 3.clj
@@ -5,8 +5,13 @@
   [nr]
   (range 2 (/ nr 2.0)))
 
-(defn is-prime?
+(defn divisible?
+  [nr div]
+  (= 0 (mod nr div)))
+
+(defn prime?
   [nr]
+  (println "prime?" nr)
   (if (or (< nr 2) (= nr 4))
     false
     (= 0
@@ -14,18 +19,19 @@
         (filter
           (fn
             [div]
-            (= 0 (mod nr div)))
-          (divisors-until nr)))
-    )
-  )
-)
+            (divisible? nr div))
+          (divisors-until nr))))))
+
+(defn primes-until
+  [nr]
+  (filter prime? (range nr)))
 
 (defn prime-factors
   [nr]
-  )
+  (let [primes (primes-until (/ nr 2.0))]  
+    (filter (fn [chk] (println "divisible" nr chk) (divisible? nr chk))
+      (reverse primes)))
+)
 
-(println (filter is-prime? (range 20)))
-
-;(defn prime-factors
-;  [nr]
-;  (reduce 
+;(println (first-prime-factor 13195))
+(println (first (reverse (prime-factors 600851475143))))
