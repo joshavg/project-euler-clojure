593e9f428d9137ee23807fd21ce3b31d52aea36f
Josha von Gizycki <josha.gizycki@triology.de>
2016-11-02 10:55:19 +0100

    solved problem with some help

--- 3.clj
+++ 3.clj
@@ -1,37 +1,20 @@
 ;The prime factors of 13195 are 5, 7, 13 and 29.
 ;What is the largest prime factor of the number 600851475143 ?
 
-(defn divisors-until
-  [nr]
-  (range 2 (/ nr 2.0)))
-
 (defn divisible?
   [nr div]
-  (= 0 (mod nr div)))
+  (zero? (rem nr div)))
 
-(defn prime?
+(defn max-prime-factor
   [nr]
-  (println "prime?" nr)
-  (if (or (< nr 2) (= nr 4))
-    false
-    (= 0
-      (count
-        (filter
-          (fn
-            [div]
-            (divisible? nr div))
-          (divisors-until nr))))))
-
-(defn primes-until
-  [nr]
-  (filter prime? (range nr)))
-
-(defn prime-factors
-  [nr]
-  (let [primes (primes-until (/ nr 2.0))]  
-    (filter (fn [chk] (println "divisible" nr chk) (divisible? nr chk))
-      (reverse primes)))
+  (let [max-check (long (Math/sqrt nr))]
+    (loop [div 2 rest-div nr]
+      (cond
+        (> div max-check) rest-div
+        (= div rest-div) rest-div
+        (divisible? rest-div div) (recur div (/ rest-div div))
+        true (recur (inc div) rest-div)
+      )))
 )
 
-;(println (first-prime-factor 13195))
-(println (first (reverse (prime-factors 600851475143))))
+(println (max-prime-factor 600851475143))
